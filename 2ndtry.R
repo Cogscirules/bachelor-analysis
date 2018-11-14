5809d2dce5d5e4c726db45afb87b8a35e715ceda
Cogscirules <anjatoernqvist@gmail.com>
2018-11-14 13:32:30 +0100

    14/11 I think I may be done

--- 2ndtry.R
+++ 2ndtry.R
@@ -3,6 +3,9 @@ p_load(stringr, rethinking, brms, dplyr, ggplot2, gridExtra, mvtnorm, rethinking
 
 
 
+#This part of the code is merely cleaning and mergin intro the resulting final DF
+
+
 # ------------------------------------ BASE CODE BASED ON 1 PATIENT AND DATASET
 
 
@@ -473,43 +476,9 @@ final_full_df = merge(full_avg_df, final_emfit_df, all = TRUE)
 
 View(final_full_df)
 
+
 #see which are not Na or NaN
 which(is.na(final_full_df$sleepmed) == FALSE)
 #se række
 final_full_df[4909,]
 
-
-
-
-# ---------------------------- MAKING MODELS
-
-
-#Model with Search term only
-M1 <- map2stan(
-  alist(
-    sentiment ~ dnorm(mu, sigma),
-    mu <- a + duration_in_sleep*sleep_score,
-    a ~ dnorm(0,1),
-    duration_in_sleep ~ dnorm(0,1),
-    sigma ~ dnorm(0,1)
-  ), data = newdf, chains = 2, cores = 2, iter = 1000, warmup = 500
-)
-precis(M1)
-
-
-m0 <- brms::brm(duration_in_bed ~ 1 + duration_in_sleep + (1|sleep_score), data = fulldf, family = gaussian, chains = 2, cores = 2)
-summary(m0)
-pp_check(m0)
-plot(marginal_effects(m0), points = T)
-
-m1 <- brms::brm(duration_in_bed ~ 1 + duration_in_sleep + (1|sleep_score), data = fulldf2, family = gaussian, chains = 2, cores = 2)
-summary(m1)
-pp_check(m1)
-plot(marginal_effects(m1), points = T)
-
-m0<-add_ic(m0,ic="waic")
-m1 <- add_ic(m1, ic="waic")
-
-compare_ic(m0, m1, ic = "waic")
-
-model_weights(m0, m1, ic = "waic")
